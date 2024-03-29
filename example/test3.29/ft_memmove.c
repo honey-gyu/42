34fa1aa9c3b684caa46f49eb0d253899dbacd643
honey9bestit@gmail.com <hyungyki@c2r12s1.42gyeongsan.kr>
2024-03-29 18:13:58 +0900

    3월 29일

--- example/test3.29/ft_memmove.c
+++ example/test3.29/ft_memmove.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyungyki <hyungyki@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/23 22:19:05 by hyungyki          #+#    #+#             */
-/*   Updated: 2024/03/29 02:18:29 by hyungyki         ###   ########.fr       */
+/*   Created: 2024/03/29 10:00:40 by hyungyki          #+#    #+#             */
+/*   Updated: 2024/03/29 10:10:32 by hyungyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -15,11 +15,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*change;
 	unsigned char	*str;
-	size_t			i;
+	size_t	i;
 
-	i = 0;
 	change = (unsigned char *)dest;
 	str = (unsigned char *)src;
+	i = 0;
 	if (change == NULL && str == NULL)
 		return (NULL);
 	if (change < str)
@@ -37,34 +37,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (change);
 }
-/*
-void    *ft_memcpy(void *dest, const void *src, size_t n)
-{
-        unsigned char   *change;
-        unsigned char   *str;
-        size_t                  i;
-
-        change = (unsigned char *)dest;
-        str = (unsigned char *)src;
-        if (change == NULL && str == NULL)
-                return (NULL);
-        i = 0;
-        while (i < n)
-        {
-                change[i] = str[i];
-                i++;
-        }
-        return (change);
-}
-
 int	main(void)
 {
 	char dest[] = "12345";
 	char *src = dest + 1;
-	printf("%p\n", (char *)ft_memmove((void *)0, (void *)0, 5 * sizeof(char)));
-	printf("--------------\n");
-	printf("%s\n", (char *)ft_memmove(src, dest, 5 * sizeof(char)));
-	printf("--------------\n");
-//	printf("%s\n", (char *)ft_memcpy(src, dest, 5 * sizeof(char)));
-//	printf("%s\n", (char *)ft_memmove(dest, src, 5 * sizeof(char)));
-}*/
+
+	printf("%s\n", (char *)ft_memmove(dest, src, 5));
+	printf("%s\n", (char *)ft_memmove(src, dest, 5));
+}
